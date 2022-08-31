57f08bc07a06265ce385af9a86b2f5297bbbadc1
Gotmished <5002@holbertonschool.com>
2022-08-30 21:13:51 -0700

    Added makefile, updated header file, initial commit task two

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ *
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->right = parent->right;
+	if (new->right != NULL)
+	{
+		new->right->parent = new;
+	}
+	parent->right = new;
+	return (new);
+}
